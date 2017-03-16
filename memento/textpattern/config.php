30285e301a7622d644859a428cea14eb3e2ee2f3
Thomas Jund <thomas@sacripant.fr>
2017-03-16 17:11:53 +0100

    htaccess: remove gzip config

--- memento/textpattern/config.php
+++ memento/textpattern/config.php
@@ -4,7 +4,7 @@ $txpcfg['user'] = 'root';
 $txpcfg['pass'] = 'root';
 $txpcfg['host'] = 'localhost';
 $txpcfg['table_prefix'] = '';
-$txpcfg['txpath'] = '/Applications/MAMP/htdocs/memento_github/memento/textpattern';
+$txpcfg['txpath'] = '/home/thomas/sites/memento-github/memento/textpattern';
 $txpcfg['dbcharset'] = 'utf8';
 
 define('my_custom_css', true);
